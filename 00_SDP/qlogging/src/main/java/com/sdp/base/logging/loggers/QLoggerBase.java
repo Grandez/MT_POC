af79c7b35e454d504fbcc4dbafd44e15241f5075
Grandez <javier.gonzalez.grandez@gmail.com>
2025-08-17 18:43:34 +0200

    Refactoring TWO Packages: SDP y MT

--- 00_SDP/qlogging/src/main/java/com/sdp/base/logging/loggers/QLoggerBase.java
+++ 00_SDP/qlogging/src/main/java/com/sdp/base/logging/loggers/QLoggerBase.java
@@ -1,10 +1,10 @@
 package com.sdp.base.logging.loggers;
 
-import com.sdp.base.logging.objects.QObject;
+import com.sdp.base.logging.objects.*;
 
 import java.util.concurrent.PriorityBlockingQueue;
 
 public abstract class QLoggerBase {
     protected final String SEP_TOK = ":";
-    public static PriorityBlockingQueue<QObject> qdat = new PriorityBlockingQueue<>(100, new QueueComparator());
+    public static PriorityBlockingQueue<String> qlog = new PriorityBlockingQueue<>();
 }
